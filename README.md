c29664bdf6e5adfc1dcd79425d1c7cb250b42c25
BonchayHi5 <bonchayouk@gmail.com>
2022-03-23 17:19:25 +0700

    deplooy

--- README.md
+++
@@ -1,3 +0,0 @@
-# stockx_clone_ui
-
-<img src="https://github.com/BonchayHi5/stockX-clone-ui-flutter/blob/main/assets/screenshot.png?raw=true" >
--- android/app/src/main/kotlin/com/example/stockx_clone/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stockx_clone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
